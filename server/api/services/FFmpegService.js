5053c304f7a40476418e008d53cc88c208163b89
Deshi Basara <simon@i-was-perfect.net>
2014-10-24 21:34:24 +0200

    FFmpeg service prototype

---
+++ server/api/services/FFmpegService.js
@@ -0,0 +1,11 @@
+/**
+ * FFmpegService.js
+ *
+ * FFmpeg service for converting uploaded files.
+ */
+module.exports = {
+
+    convert: function(filename, inputPath, outputPath, profile) {
+
+    }
+};
