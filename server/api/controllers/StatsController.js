70bc388b4147b49853dabc3789a8fb84a1d346c5
Deshi Basara <simon@i-was-perfect.net>
2014-11-19 13:14:18 +0100

    Send all stats, don't paginate

--- server/api/controllers/StatsController.js
+++ server/api/controllers/StatsController.js
@@ -11,16 +11,22 @@ module.exports = {
      */
     all: function(req, res) {
 
-        console.log(req.body.page);
-
         //@todo check auth
 
-        if(req.body.page === undefined) {
+        /*if(req.body.page === undefined) {
             return res.send(400, 'Bad request');
         }
 
         // sort descending, limit to 20 enties, skip for pagination
         Stats.find().sort({ createdAt: 'desc' }).skip(req.body.page * 15).limit(15).exec(function(err, stats) {
+            if(err) {
+                return res.send(500, err);
+            }
+
+            res.send(stats);
+        });*/
+
+        Stats.find().sort({ createdAt: 'desc' }).exec(function(err, stats) {
             if(err) {
                 return res.send(500, err);
             }
