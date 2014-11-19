550df32ceb4e1a3c770efa3d862dc77076fe0ad0
Deshi Basara <simon@i-was-perfect.net>
2014-11-19 13:14:34 +0100

    ngTable params with pagination

--- client/app/scripts/routes/queue/queue.controller.js
+++ client/app/scripts/routes/queue/queue.controller.js
@@ -6,41 +6,75 @@
         .module('app')
         .controller('QueueCtrl', QueueCtrl);
 
-    QueueCtrl.$inject = ['QueueService'];
+    QueueCtrl.$inject = ['QueueService', 'ngTableParams'];
 
     /**
      * Handles the dash-board view and all interactions
      */
-    function QueueCtrl(QueueService) {
+    function QueueCtrl(QueueService, ngTableParams) {
         var ctrl = this;
 
-        var data = [
-            {id: 1, status: 'queued', prio: 'high', name: 'GLF-Campus 720p', profile: 'ogv 720p', pass: 1, created: '12.03.1991', user: 'Ada Rhode'},
-            {id: 1, status: 'queued', prio: 'high', name: 'GLF-Campus 720p', profile: 'ogv 720p', pass: 1, created: '12.03.1991', user: 'Ada Rhode'},
-            {id: 1, status: 'queued', prio: 'high', name: 'GLF-Campus 720p', profile: 'ogv 720p', pass: 1, created: '12.03.1991', user: 'Ada Rhode'},
-            {id: 1, status: 'queued', prio: 'high', name: 'GLF-Campus 720p', profile: 'ogv 720p', pass: 1, created: '12.03.1991', user: 'Ada Rhode'},
-            {id: 1, status: 'queued', prio: 'high', name: 'GLF-Campus 720p', profile: 'ogv 720p', pass: 1, created: '12.03.1991', user: 'Ada Rhode'},
-            {id: 1, status: 'queued', prio: 'high', name: 'GLF-Campus 720p', profile: 'ogv 720p', pass: 1, created: '12.03.1991', user: 'Ada Rhode'},
-            {id: 1, status: 'queued', prio: 'high', name: 'GLF-Campus 720p', profile: 'ogv 720p', pass: 1, created: '12.03.1991', user: 'Ada Rhode'},
-            {id: 1, status: 'queued', prio: 'high', name: 'GLF-Campus 720p', profile: 'ogv 720p', pass: 1, created: '12.03.1991', user: 'Ada Rhode'},
-            {id: 1, status: 'queued', prio: 'high', name: 'GLF-Campus 720p', profile: 'ogv 720p', pass: 1, created: '12.03.1991', user: 'Ada Rhode'},
-            {id: 1, status: 'queued', prio: 'high', name: 'GLF-Campus 720p', profile: 'ogv 720p', pass: 1, created: '12.03.1991', user: 'Ada Rhode'}
-        ];
+        /**
+         * Fetches all finished videos and starts a remove request.
+         */
+        function removeAllFinished() {
+            // get all finished
+            var finishedArray = [];
+            for (var i = 0; i < ctrl.queueData.length; i++) {
+                var currentVideo = ctrl.queueData[i];
+
+                // check if it's finished
+                if(currentVideo.status === 'finished') {
+                    finishedArray.push(currentVideo);
+                }
+            };
+
+            console.log(finishedArray);
+        }
+
+
 
         //////////////////////
 
-        QueueService.getAllInQueue(0).then(function(success) {
-            console.log(success);
-            ctrl.queueData = success;
-        }, function(error) {
+        // ngTable settings
+        var tableParams = new ngTableParams({
+            page: 1,
+            count: 15
+        }, {
+            total: 0,
+            counts: [],
+            getData: function($defer, params) {
+                // fetch the table data
+                QueueService.getAllInQueue().then(function(success) {
+                    // update the table params
+                    params.total(success.length);
+                    // slice and set new data for the current page
+                    var data = success.slice((params.page() - 1) * params.count(), params.page() * params.count());
+                    $defer.resolve(data);
 
+                    // save result globally
+                    ctrl.queueData = success;
+
+                }, function(error) {
+
+                });
+            }
         });
 
         //////////////////////
 
         angular.extend(ctrl, {
-            queueData: null
+            queueData: null,
+            tableParams: tableParams,
+
+            removeAllFinished: removeAllFinished
         });
+
+        //////////////////////
+
+        //////////////////////
+
+
     }
 
 })();
--- client/app/scripts/routes/queue/queue.service.js
+++ client/app/scripts/routes/queue/queue.service.js
@@ -24,23 +24,16 @@
         ///////////////
 
         /**
-         * Sends a request with the currentPage to the server for fetching all stats-entries at
-         * the currentPage.
-         * @param  {[int]}    currentPage  [Pagination index]
+         * Sends a request with to the server for fetching all stats-entries at.
          * @return {Promise}          [Resolve: true | Reject: false]
          */
-        function getAllInQueue(currentPage) {
+        function getAllInQueue() {
             var q = $q.defer();
-
-            console.log(currentPage);
 
             // make the request
             $http({
-                method: 'POST',
-                url: config.apiUrl + service.allUrl,
-                data: {
-                    page: currentPage
-                }
+                method: 'GET',
+                url: config.apiUrl + service.allUrl
             }).success(function(data) {
                 q.resolve(data);
             }).error(function(data, status) {
