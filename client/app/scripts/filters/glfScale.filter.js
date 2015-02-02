4038da9e76a845cab7df8ed3412235f0809c951e
Deshi Basara <simon@i-was-perfect.net>
2015-02-02 15:06:07 +0100

    ScaleFactor filter added

---
+++ client/app/scripts/filters/glfScale.filter.js
@@ -0,0 +1,37 @@
+(function() {
+
+    'use strict';
+
+    angular
+        .module('app')
+        .filter('glfScale', glfScale);
+
+    /**
+     * Filter for converting ffmpeg scale values into human readable values
+     */
+    function glfScale() {
+        return function(factor) {
+            console.log(factor);
+            if(factor === 'scale=-1:-1') {
+                return 'Untouched';
+            }
+            else if(factor === 'scale=-1:1080') {
+                return '1080p';
+            }
+            else if(factor === 'scale=-1:720') {
+                return '720p';
+            }
+            else if(factor === 'scale=-1:480') {
+                return '480p';
+            }
+            else if(factor === 'scale=-1:360') {
+                return '360p';
+            }
+            else {
+                return '-'
+            }
+        };
+    }
+
+
+})();
